44c8120a1b59e15f0b3eb4cc73333409b766a45f
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-03-10 11:18:59 +0100

    refactor: refactor of Pattern 3

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -2,8 +2,12 @@ varying vec2 vUv;
 
 void main()
 {
+    // Grayscale value
+    float strength = vUv.x;
+
     // gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0); Base Pattern
     // gl_FragColor = vec4(vUv, 1.0, 1.0); Pattern 1
     // gl_FragColor = vec4(vUv, 0.0, 1.0); Pattern 2
-    gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0);
+    // gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0); Pattern 3
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
