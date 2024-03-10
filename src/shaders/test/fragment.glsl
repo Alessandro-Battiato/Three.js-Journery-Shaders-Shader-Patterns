99d0389acab8cfa8ae6bd4c0897dd3c4abe6a35f
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-03-10 11:19:23 +0100

    feat: pattern 4

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -3,7 +3,7 @@ varying vec2 vUv;
 void main()
 {
     // Grayscale value
-    float strength = vUv.x;
+    float strength = vUv.y;
 
     // gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0); Base Pattern
     // gl_FragColor = vec4(vUv, 1.0, 1.0); Pattern 1
